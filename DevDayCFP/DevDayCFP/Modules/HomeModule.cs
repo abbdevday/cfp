d00ff478044b3fe47e057bc10da5769e1637127d
Biegal <marcin.biegala@gmail.com>
2015-01-24 06:55:17 +0100

    'Hello World' working views

---
+++ DevDayCFP/DevDayCFP/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace DevDayCFP.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["Index"];
+        }
+    }
+}
