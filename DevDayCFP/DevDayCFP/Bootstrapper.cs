2ab015530caf4b7954a64b26dd06ea82e56fb98d
Biegal <marcin.biegala@gmail.com>
2015-01-24 21:59:02 +0100

    Some basic layout

---
+++ DevDayCFP/DevDayCFP/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace DevDayCFP
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("scripts", @"Scripts"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("images", @"Images"));
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("fonts", @"Fonts"));
+        }
+    }
+}
