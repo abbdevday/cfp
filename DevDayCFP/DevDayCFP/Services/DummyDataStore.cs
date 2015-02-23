deb12d2fa3478ed7d1a83f74121016bf1e106b6c
Marcin Biegala <marcin.biegala@pl.abb.com>
2015-02-23 07:43:21 +0100

    Simple.Data added as nuget for data retrieval

--- DevDayCFP/DevDayCFP/Services/DummyDataStore.cs
+++ DevDayCFP/DevDayCFP/Services/DummyDataStore.cs
@@ -6,39 +6,39 @@ using Nancy.Security;
 
 namespace DevDayCFP.Services
 {
-    public class DummyDataStore : IDataStore
-    {
-        private readonly List<User> _usersStore = new List<User>();
-        private readonly List<Paper> _papersStore = new List<Paper>(); 
+    //public class DummyDataStore : IDataStore
+    //{
+    //    private readonly List<User> _usersStore = new List<User>();
+    //    private readonly List<Paper> _papersStore = new List<Paper>(); 
 
-        public IUserIdentity GetUserById(Guid identifier)
-        {
-            return _usersStore.Single(u => u.Id == identifier);
-        }
+    //    public IUserIdentity GetUserById(Guid identifier)
+    //    {
+    //        return _usersStore.Single(u => u.Id == identifier);
+    //    }
 
-        public User GetUserByLoginData(string userName, string password)
-        {
-            return _usersStore.SingleOrDefault(u => u.UserName == userName && u.Password == password);
-        }
+    //    public User GetUserByLoginData(string userName, string password)
+    //    {
+    //        return _usersStore.SingleOrDefault(u => u.UserName == userName && u.Password == password);
+    //    }
 
-        public User GetUserByLoginOrEmail(string userName, string email)
-        {
-            return _usersStore.SingleOrDefault(u => u.UserName == userName || u.Email == email);
-        }
+    //    public User GetUserByLoginOrEmail(string userName, string email)
+    //    {
+    //        return _usersStore.SingleOrDefault(u => u.UserName == userName || u.Email == email);
+    //    }
 
-        public void SaveUser(User userRecord)
-        {
-            _usersStore.Add(userRecord);
-        }
+    //    public void SaveUser(User userRecord)
+    //    {
+    //        _usersStore.Add(userRecord);
+    //    }
 
-        public IEnumerable<Paper> GetPapersByUser(string userName)
-        {
-            return _papersStore;
-        }
+    //    public IEnumerable<Paper> GetPapersByUser(string userName)
+    //    {
+    //        return _papersStore;
+    //    }
 
-        public Paper GetPaperById(Guid id)
-        {
-            return _papersStore.FirstOrDefault(p => p.Id == id);
-        }
-    }
+    //    public Paper GetPaperById(Guid id)
+    //    {
+    //        return _papersStore.FirstOrDefault(p => p.Id == id);
+    //    }
+    //}
 }
---
+++ DevDayCFP/DevDayCFP/Services/SimpleDataStore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DevDayCFP.Models;
+using Nancy.Security;
+using Simple.Data;
+
+namespace DevDayCFP.Services
+{
+    public class SimpleDataStore : IDataStore
+    {
+        public IUserIdentity GetUserById(Guid identifier)
+        {
+            var db = Database.OpenNamedConnection("DefaultConnection");
+            var user = db.Users.Get(identifier);
+            return user as IUserIdentity;
+        }
+
+        public User GetUserByLoginData(string userName, string password)
+        {
+            var db = Database.OpenNamedConnection("DefaultConnection");
+            var user = db.Users.FindByUserName(userName).FirstOrDefault();
+            return user;
+        }
+
+        public User GetUserByLoginOrEmail(string userName, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveUser(User userRecord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Paper> GetPapersByUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Paper GetPaperById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
