50449b0fdafa1c7dffa320c1fe882464808a0476
Marcin Biegala <marcin.biegala@pl.abb.com>
2015-05-11 07:35:41 +0200

    #12: More password requirements

--- DevDayCFP/DevDayCFP/ViewModels/RegisterViewModel.cs
+++ DevDayCFP/DevDayCFP/ViewModels/RegisterViewModel.cs
@@ -15,6 +15,7 @@ namespace DevDayCFP.ViewModels
 
         [Required]
         [StringLength(128, MinimumLength = 8, ErrorMessage = "Password lengths must be between 8 and 128 characters in length")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,128}$", ErrorMessage = "Password must contain at least: one lowercase and one uppercase letter, one digit and be minimum 8 characters long")]
         public string Password { get; set; }
 
         [Required]
